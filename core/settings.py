019af0346f402d98c80ef049cd0dbc0714a0999f
laushkin1 <laushkinivan1@gmail.com>
2026-07-20 19:02:27 +0200

    feat: add basic disigne

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
